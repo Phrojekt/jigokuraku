bef5a2ed079d8761d720c5071310f471bf617503
Phrojekt <phrojekt22@gmail.com>
2024-07-30 22:30:51 -0300

    ..

--- components/ContentBlock.tsx
+++ components/ContentBlock.tsx
@@ -10,9 +10,9 @@ export function ContentBlock({title, description}: ContentBlockProps) {
 
     return (
         <div className="flex gap-x-0">
-            <div className="bg-light-gray sm:w-40 md:w-80 h-[850px] rounded-3xl rounded-r-none pl-8 pt-6">
+            <div className="bg-light-gray sm:w-40 md:w-80 h-[850px] rounded-3xl rounded-r-none pl-8 pt-6 space-y-8">
                 <h2 className="text-black text-5xl font-alt">{title}</h2>
-                <div >{description}</div>
+                <div className="text-black text-normal" >{description}</div>
             </div>
             <div className="bg-dark-gray sm:w-40 md:w-80 h-[850px] rounded-3xl rounded-l-none"></div>
         </div>
--- components/ContentUseState.tsx
+++ components/ContentUseState.tsx
@@ -7,8 +7,8 @@ import ContentBlock from "./ContentBlock";
 export function ContentUseState() {
 
     const array  = [
-        {title: 'Gabimaru', description: 'Sexo é bom sexo é bom'},
-        {title: 'Yamada Sagiri', description: 'Sexo é ruim sexo é ruim'}
+        {title: 'Gabimaru', description: 'Gabimaru, nascido como Saku, é um ninja de elite de Iwagakure e o último detentor do pseudônimo Gabimaru, o Vazio. É o principal protagonista desta obra, sendo um ninja que foi traído pelo próprio vilarejo após decidir renunciar da função de assassino no propósito de viver uma vida pacífica com sua esposa, Yui. Devido a traição, Gabimaru foi entregue as autoridades para que seja executado e desta forma acabou conhecendo Sagiri do clã Yamada, um clã responsável por executar criminosos sob o comando direto do Shogun. Através de Yamada Sagiri, o assassino recebeu o convite do Shogun para encontrar o Elixir da vida em troca do perdão de todos os seus crimes. '},
+        {title: 'Yamada Sagiri', description: 'Nascida e criada em um ambiente marcado pela violência e pela morte, Sagiri internalizou os valores e as expectativas de seu clã. Sua vida gira em torno do domínio da espada e do cumprimentod e sua missão como carrasca. No entanto, por trás de sua fachada implacável, Sagiri revela uma complexidade emocional que a diferencia dos outros membros de seu clã. Como membro do clã Yamada, Sagiri foi enviada para a busca pelo Elixir da Vida como responsável do criminoso Gabimaru, sob ordem do Shogun. Ao longo da história, Sagiri se desenvolve, aprendendo a lidar com as consequências de suas ações e a encontrar um propósito além da violência e do perfeccionismo gerado das expectativas alheias.'}
     ]
 
     const [currentItem, setCurrentItem] = useState(0)
