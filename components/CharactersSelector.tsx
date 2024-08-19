d5e5a123609c5c29141936cb96c73b6cce7a2a5c
Phrojekt <phrojekt22@gmail.com>
2024-08-19 16:21:22 -0300

    ..

--- components/CharactersSelector.tsx
+++ components/CharactersSelector.tsx
@@ -7,7 +7,7 @@ type CharactersSelectorProps  = {
 
 export function CharactersSelector({handleSetArray}: CharactersSelectorProps) {
     return ( 
-        <div className="flex flex-row sm:flex-nowrap md:flex-wrap sm:gap-1 md:gap-x-4 md:overflow-hidden sm:w-[200%] h-full md:w-[100%]">
+        <div className="flex flex-row sm:flex-nowrap md:flex-wrap sm:gap-1 md:gap-0 md:gap-x-2 md:pt-[40px] md:pl-[20px] md:pt-[20px] md:overflow-hidden sm:w-[200%] h-full md:w-[100%]">
                         <div className="">
                             <Image src="/images/Gabimaru.png" alt="Gabimaru Icon" width={500} height={500} quality={100} className="md:w-[100px] md:h-[100px] md:hover:-translate-y-2 md:hover:scale-110 sm:rounded-xl md:rounded-3xl cursor-pointer" role="button" onClick={() => handleSetArray(0)}/>
                         </div>
