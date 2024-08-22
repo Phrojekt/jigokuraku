4ffdbe27d88be39cdc7a05bfd6ff95fcec8cbce7
Phrojekt <phrojekt22@gmail.com>
2024-08-21 23:13:21 -0300

    ..

--- components/ContentBlock.tsx
+++ components/ContentBlock.tsx
@@ -19,7 +19,7 @@ export function ContentBlock({title, description, description2, url}: ContentBlo
             <div className="bg-dark-gray sm:w-40 sm:w-[100%] sm:h-[208px] sm:rounded-t-3xl sm:rounded-tl-3xl sm:rounded-b-none md:w-80 md:h-[850px] md:rounded-3xl md:rounded-l-none flex sm:flex-col md:flex-col-reverse overflow-hidden">
                 {url && (
                     <Image 
-                    src={url} alt="aiai" width={500} height={500} quality={100} className=" md:w-[380px] sm:relative sm:top-[-70px] sm:left-[60px]" />
+                    src={url} alt="aiai" width={500} height={500} quality={100} className=" md:w-[380px] sm:relative sm:top-[-65px] sm:left-[50px] md:top-[0px] md:left-[0px]" />
                 )}
             </div>
         </div>
